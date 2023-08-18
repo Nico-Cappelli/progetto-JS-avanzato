d5710b44bdbcd4cfa28353b90f304c897c0ce1a6
Nico-Cappelli <nicocappelli.nc@gmail.com>
2023-08-18 10:45:01 +0200

    buona

--- script/script.js
+++ script/script.js
@@ -4,8 +4,20 @@ let takeCat = document.getElementById("categories");
 let takeSco = document.getElementById("score");
 
 
+ 
 
- const btn = document.querySelector(".btn");
+fetch('https://api.teleport.org/api/urban_areas/slug:los-angeles/scores/')
+.then(response => response.json())
+.then(json=>{
+  const infoFetch = json;
+  const btn = document.querySelector("#btn");
+  btn.addEventListener("click", function writeInfo(){
+    let inputVal = document.getElementById("text").value;
+    if (inputVal != "Los Angeles") {
+      alert("insert the correct city")
+      
+    } else{
+      const btn = document.querySelector(".btn");
  
        btn.addEventListener("click", function () {
          takeSum.style.display = "block";
@@ -14,32 +26,18 @@ let takeSco = document.getElementById("score");
 
         });
 
-fetch('https://api.teleport.org/api/urban_areas/slug:los-angeles/scores/')
-.then(response => response.json())
-.then(json=>{
- const infoFetch = json;
 
-
-
-
- 
-     
- const btn = document.querySelector("#btn");
-  btn.addEventListener("click", function writeInfo(){
-   
-        takeSum.innerHTML = (infoFetch.summary);
-        let tc = infoFetch.teleport_city_score.toFixed(2);
-        takeSco.innerHTML = ("Total Score: " + tc);
+      takeSum.innerHTML = (infoFetch.summary);
+      let tc = infoFetch.teleport_city_score.toFixed(2);
+      takeSco.innerHTML = ("Total Score: " + tc);
 
 infoFetch.categories.forEach((x) => {
-    takeCat.insertAdjacentHTML(
-      "afterbegin",
-      `${x.name}: ${x.score_out_of_10.toFixed(2)}<br>`
-    );
-  });
+  takeCat.insertAdjacentHTML("afterbegin",`${x.name}: ${x.score_out_of_10.toFixed(2)}<br>`);
+});
+    };
+   
   
 
-}) 
-   writeInfo();
+});
 });
 
