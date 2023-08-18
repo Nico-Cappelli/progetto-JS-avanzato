1909ba06c2151958ec91932e1b3966e4ca0831d9
Nico-Cappelli <nicocappelli.nc@gmail.com>
2023-08-18 12:14:31 +0200

    ottimo

--- script/script.js
+++ script/script.js
@@ -1,10 +1,20 @@
 
-let takeSum = document.getElementById("summary");
+/*let takeSum = document.getElementById("summary");
 let takeCat = document.getElementById("categories");
-let takeSco = document.getElementById("score");
+let takeSco = document.getElementById("score");*/
 
-
+function create(divValue, element, clas, attribute)
+{
+  const contenitore = document.getElementById(divValue);
+  const NEW_ELEMENT = document.createElement(element);
  
+NEW_ELEMENT.classList.add(clas);
+NEW_ELEMENT.id = (attribute);
+contenitore.appendChild(NEW_ELEMENT);
+
+return NEW_ELEMENT;
+};
+
 
 fetch('https://api.teleport.org/api/urban_areas/slug:los-angeles/scores/')
 .then(response => response.json())
@@ -13,31 +23,36 @@ fetch('https://api.teleport.org/api/urban_areas/slug:los-angeles/scores/')
   const btn = document.querySelector("#btn");
   btn.addEventListener("click", function writeInfo(){
     let inputVal = document.getElementById("text").value;
-    if (inputVal != "Los Angeles") {
-      alert("insert the correct city")
-      
-    } else{
-      const btn = document.querySelector(".btn");
- 
-       btn.addEventListener("click", function () {
-         takeSum.style.display = "block";
-         takeCat.style.display = "block";
-         takeSco.style.display = "block";
+    if (inputVal == "Los Angeles") {
+      const takeSum = create("bodyDiv", "div", "parag", "summary" );
+      const takeCat = create("bodyDiv", "div", "parag", "categories" );
+      const takeSco = create("bodyDiv", "div", "parag", "score" );
+      /*btn.addEventListener("click", function () {
+        takeSum.style.display = "block";
+        takeCat.style.display = "block";
+        takeSco.style.display = "block";
 
-        });
+       });*/
 
 
-      takeSum.innerHTML = (infoFetch.summary);
-      let tc = infoFetch.teleport_city_score.toFixed(2);
-      takeSco.innerHTML = ("Total Score: " + tc);
+     takeSum.innerHTML = (infoFetch.summary);
+     let tc = infoFetch.teleport_city_score.toFixed(2);
+     takeSco.innerHTML = ("Total Score: " + tc);
 
 infoFetch.categories.forEach((x) => {
-  takeCat.insertAdjacentHTML("afterbegin",`${x.name}: ${x.score_out_of_10.toFixed(2)}<br>`);
+ takeCat.insertAdjacentHTML("afterbegin",`${x.name}: ${x.score_out_of_10.toFixed(2)}<br>`);
 });
+      
+      
+    } else{
+ 
+       alert("insert the correct city")
     };
    
   
 
 });
 });
+
+
 
